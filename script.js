433467e95e36a68bd6cd87765f7c36b33d1846bc
thmasibtagiyev0@gmail.com <tahmasibmt@code.edu.az>
2024-06-10 18:22:21 +0400

    first commit

---
+++ script.js
@@ -0,0 +1,106 @@
+
+
+document.addEventListener('DOMContentLoaded', (event) => {
+    const li1 = document.getElementById('li1');
+    const d1 = document.getElementById('d-1');
+
+    li1.addEventListener('mouseenter', () => {
+        d1.style.display = 'block';
+    });
+
+    li1.addEventListener('mouseleave', () => {
+        setTimeout(() => {
+            if (!d1.matches(':hover')) {
+                d1.style.display = 'none';
+            }
+        }, 200);
+    });
+
+    d1.addEventListener('mouseleave', () => {
+        d1.style.display = 'none';
+    });
+});
+
+document.addEventListener('DOMContentLoaded', (event) => {
+    const li2=document.getElementById("li2");
+    const d2=document.getElementById("d-2");
+
+    li2.addEventListener("mouseenter", () =>{
+        d2.style.display = "block";
+    });
+
+    li2.addEventListener("mouseleave", () => {
+            setTimeout(() =>{
+                if (!d2.matches(":hover")) {
+                    d2.style.display = "none";
+                }
+            },200)
+        }); 
+
+        d2.addEventListener("mouseleave", () =>{
+            d2.style.display = "none";
+    });
+});
+
+document.addEventListener('DOMContentLoaded', (event) => {
+    const li3=document.getElementById("li3");
+    const d3=document.getElementById("d-3");
+
+    li3.addEventListener("mouseenter", () =>{
+        d3.style.display = "block";
+    });
+
+    li3.addEventListener("mouseleave", () => {
+            setTimeout(() =>{
+                if (!d3.matches(":hover")) {
+                    d3.style.display = "none";
+                }
+            },200)
+        }); 
+
+        d3.addEventListener("mouseleave", () =>{
+            d3.style.display = "none";
+    });
+});
+
+document.addEventListener('DOMContentLoaded', (event) => {
+    const li5=document.getElementById("li5");
+    const d5=document.getElementById("d-5");
+
+    li5.addEventListener("mouseenter", () =>{
+        d5.style.display = "block";
+    });
+
+    li5.addEventListener("mouseleave", () => {
+            setTimeout(() =>{
+                if (!d5.matches(":hover")) {
+                    d5.style.display = "none";
+                }
+            },200)
+        }); 
+
+        d5.addEventListener("mouseleave", () =>{
+            d5.style.display = "none";
+    });
+});
+
+document.addEventListener('DOMContentLoaded', (event) => {
+    const li6=document.getElementById("li6");
+    const d6=document.getElementById("d-6");
+
+    li6.addEventListener("mouseenter", () =>{
+        d6.style.display = "block";
+    });
+
+    li6.addEventListener("mouseleave", () => {
+            setTimeout(() =>{
+                if (!d6.matches(":hover")) {
+                    d6.style.display = "none";
+                }
+            },200)
+        }); 
+
+        d6.addEventListener("mouseleave", () =>{
+            d6.style.display = "none";
+    });
+});
