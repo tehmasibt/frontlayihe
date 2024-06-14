ff5344f79e6c2dacbd07bdf78d9da89fa77d63aa
thmasibtagiyev0@gmail.com <tahmasibmt@code.edu.az>
2024-06-14 18:18:29 +0400

    sd

--- script.js
+++ script.js
@@ -1,8 +1,5 @@
 AOS.init();
 
-
-
-
 document.addEventListener('DOMContentLoaded', (event) => {
     const li1 = document.getElementById('li1');
     const d1 = document.getElementById('d-1');
@@ -127,5 +124,6 @@ function showSlides() {
   }
   slides[slideIndex-1].style.display = "block";  
   dots[slideIndex-1].className += " active";
-  setTimeout(showSlides, 3000); // Change image every 2 seconds
+  setTimeout(showSlides, 3000); 
 }
+
