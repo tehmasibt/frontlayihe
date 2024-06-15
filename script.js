2dcb642a30974a95e3ad90e7fca95c755bfc7ded
thmasibtagiyev0@gmail.com <tahmasibmt@code.edu.az>
2024-06-15 23:56:38 +0400

    Carousel

--- script.js
+++ script.js
@@ -104,37 +104,74 @@ document.addEventListener('DOMContentLoaded', (event) => {
             d6.style.display = "none";
     });
 });
-// todo  
-let slideIndex = 0;
-showSlides();
-
-function showSlides() {
-  let i;
-  let slides = document.getElementsByClassName("mySlides");
-  let dots = document.getElementsByClassName("dot");
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";  
-  }
-  slideIndex++;
-  if (slideIndex > slides.length) {slideIndex = 1}    
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex-1].style.display = "block";  
-  dots[slideIndex-1].className += " active";
-  setTimeout(showSlides, 3000); 
-}
 
 // ?
-function toggle(event) {
-    event.preventDefault();
-    var img=document.querySelector(".thirdsect .chose-image");
-
-    if(img.style.display === "none"){
-        img.style.display = "block"
-    }
-    else{
-        img.style.display = "none";
-    }
+const i2=document.querySelector("#cup2");
+const choseImage1=document.querySelector(".chose-image1");
+i2.onclick=function() {
+    choseImage1.style.display="none";
+}
+const i1=document.querySelector("#cup1");
+const choseImage2=document.querySelector(".chose-image2");
+i1.onclick=function() {
+    choseImage2.style.display="none";
 }
 
+// * =============================
+const wrapper=document.querySelector(".wrapper");
+const carousel=document.querySelector(".carousel");
+const arrowBtns=document.querySelectorAll(".wrapper i");
+// const firstCardWidth=carousel.querySelector(".card").offsetWidth;
+// const carouselChildrens=[...carousel.children];
+// let isDragging = false, startX, startScrollLeft, timeoutId;
+// let cardPerView = Math.round(carousel.offsetWidth / firstCardWidth);
+// carouselChildrens.slice(-cardPerView).reverse().forEach(card => {
+//     carousel.insertAdjacentHTML("afterbegin", card.outerHTML)
+// });
+// carouselChildrens.slice(0, cardPerView).forEach(card => {
+//     carousel.insertAdjacentHTML("beforeend", card.outerHTML)
+// });
+// arrowBtns.forEach(btn => {
+//     btn.addEventListener("click", () => {
+//         carousel.scrollLeft += btn.id === "left" ? - firstCardWidth : firstCardWidth;
+//     })
+// })
+// const dragStart = (e) =>{
+//     isDragging = true;
+//     carousel.classList.add("dragging");  
+//     startX = e.pageX;
+//     startScrollLeft = carousel.scrollLeft;           
+// }
+// const dragging= (e)=>{
+//     if(!isDragging) return;
+//     carousel.scrollLeft =startScrollLeft - (e.pageX-startX);
+// }
+// const dragStop= ()=>{
+//     isDragging=false;
+//     carousel.classList.remove("dragging")
+// }
+// const autoPlay = () =>{
+//     if(window.innerWidth < 800) return;
+//     timeoutId = setTimeout(()=>carousel.scrollLeft += firstCardWidth, 2000)
+// }
+// autoPlay();
+// const infiniteScroll= ()=> {
+//     if (carousel.scrollLeft === 0) {
+//         carousel.classList.add("no-transition");
+//         carousel.scrollLeft = carousel.scrollWidth - ( 2 * carousel.offsetWidth);
+//         carousel.classList.remove("no-transition");
+//     }
+//     else if(Math.ceil(carousel.scrollLeft) === carousel.scrollWidth - carousel.offsetWidth){
+//         carousel.classList.add("no-transition");
+//         carousel.scrollLeft= carousel.offsetWidth;
+//         carousel.classList.remove("no-transition");
+//     }
+//     clearTimeout(timeoutId);
+//     if(!wrapper.matches(":hover")) autoPlay();
+// }
+// carousel.addEventListener("mousedown", dragStart);
+// carousel.addEventListener("mousemove", dragging);
+// document.addEventListener("mouseup", dragStop);
+// carousel.addEventListener("scroll", infiniteScroll);
+// wrapper.addEventListener("mouseenter", ()=> clearTimeout (timeoutId));
+// carousel.addEventListener("mouseleave", autoPlay);
